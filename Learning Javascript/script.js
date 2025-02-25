ef2ac21624b67830fe4916a084f6185591329368
clintjeff2 <clintjeff2@gmail.com>
2025-02-25 15:58:19 +0100

    Day One of javascript

---
+++ Learning Javascript/script.js
@@ -0,0 +1,117 @@
+//COMMENTS
+//console.log('Logging');
+/*console.log("This is something");
+console.log("This is something");
+console.log("This is something");
+*/
+
+//VARIABLES
+/*var x;
+var y;
+var name;
+var myHouseMate;
+
+let house;
+let fruit = 'Pear';
+console.log(fruit);
+
+const pi = 3.14;
+const identity = 'LETTERS';
+
+
+
+x = 20;
+y = 31;
+name = 'Ngwa';
+
+house = 'Pent House';
+fruit = 'Apples';
+myHouseMate = 'Anna';
+
+console.log(pi);
+
+pi = 5;
+console.log(pi);
+
+console.log(identity);
+console.log(house, fruit, myHouseMate, name, x, y);
+*/
+
+//OPERATORS
+/*let num1 = 10;
+let num2 = 6;
+let num3 = 8;
+
+let answer = num1 + num2;
+console.log(answer);
+
+let newNumber = 12;
+// answer = newNumber + answer;
+answer += newNumber;
+console.log(answer);
+
+let number = 30;
+number++;
+console.log(number);
+number++;
+console.log(number);
+number++;
+console.log(number);
+*/
+
+//DATATYPES
+/*
+let fruit = 'Apples';
+let amount = 2000;
+let pi = 3.14;
+
+let letter = 'Z';
+let isSunny = true;
+
+console.log(typeof fruit);
+console.log(typeof isSunny);
+
+let person = { height: 1.8, color: 'dark', weight: 60 };
+console.log(typeof person);
+
+let height = 160;
+let allHeights = [140, 160, 130, 160, 170];
+console.log(typeof allHeights);
+
+let carName;
+console.log(typeof carName);
+*/
+
+//FUNCTIONS
+/*function functionName(){
+  //code here
+}*/
+//Function that tells me how to eat
+function howToEat() {
+	console.log('1. Collect the Food');
+	console.log('2. Wash your hands');
+	console.log('3. Choose your cutlery');
+	console.log('4. Carry food with the cutlery');
+	console.log('5. Put in your mouth');
+	console.log('6. Chew and Swallow');
+}
+
+//Assume you've seen the food
+howToEat();
+
+//Function to add two number
+function addNumbers() {
+	let num1 = 10;
+	let num2 = 20;
+
+	console.log(num1 + num2);
+}
+
+addNumbers();
+
+//Function to add ANY two numbers
+function addAnyNumbers(number1, number2) {
+	console.log(number1 + number2);
+}
+addAnyNumbers(40, 23);
+addAnyNumbers(780, 23);
