f5aaa34080e7adc0144a37bffd5b689c417de778
clintjeff2 <clintjeff2@gmail.com>
2025-03-04 12:39:23 +0100

    Finished with basic DOM manipulation and Events. Also introduced HTML tables for the Calculator project

--- Learning Javascript/dom.js
+++ Learning Javascript/dom.js
@@ -18,6 +18,25 @@ let elements1 = document.querySelector('.element');
 console.log(elements1);
 
 //SELECTING BY QUERY ALL
-
 let elements12 = document.querySelectorAll('.element');
 console.log(elements12);
+
+//SELECTING ELEMENTS BY TAG NAME
+let allTags = document.getElementsByTagName('div');
+console.log(allTags);
+
+function modifyElement() {
+	element1.innerHTML = 'I WAS CLICKED';
+	element1.style.color = 'orange';
+	element1.style.backgroundColor = 'black';
+}
+
+//EVENTS WITH DOM
+//click, mouseover, mousedown, mouseup, change, focus, load, keypressed, keydown, keyup
+element1.onload = modifyElement;
+
+element2.addEventListener('click', function () {
+	element1.innerHTML = 'I WAS CLICKED';
+	element1.style.color = 'orange';
+	element1.style.backgroundColor = 'black';
+});
